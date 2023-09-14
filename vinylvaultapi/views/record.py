98c2588927410d6a2a633ca7b83fb49fcefb2498
Mark Hamilton <ham.mark126@gmail.com>
2023-09-13 20:32:19 -0500

    search added

--- vinylvaultapi/views/record.py
+++ vinylvaultapi/views/record.py
@@ -2,12 +2,14 @@ from django.http import HttpResponseServerError
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework.decorators import action
-from rest_framework import status
+from rest_framework import status, filters
 from vinylvaultapi.models import Record, User, Genre, WishlistRecord, BorrowedRecord
 from vinylvaultapi.serializers import RecordSerializer
 
 class RecordView(ViewSet):
     """Vinyl Vault Record View"""
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['name', 'artist']
     def retrieve(self, request, pk):
         """GET request for a single record"""
         try:
