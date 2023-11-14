7b11d5a85b168c5e423fcb13cb3a243865e51dd0
Mark Hamilton <ham.mark126@gmail.com>
2023-11-13 23:45:22 -0600

    heroku changes

--- vinylvault/settings.py
+++ vinylvault/settings.py
@@ -95,6 +95,11 @@ WSGI_APPLICATION = 'vinylvault.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 if IS_HEROKU_APP:
+    # In production on Heroku the database configuration is derived from the `DATABASE_URL`
+    # environment variable by the dj-database-url package. `DATABASE_URL` will be set
+    # automatically by Heroku when a database addon is attached to your Heroku app. See:
+    # https://devcenter.heroku.com/articles/provisioning-heroku-postgres
+    # https://github.com/jazzband/dj-database-url
     DATABASES = {
         "default": dj_database_url.config(
             conn_max_age=600,
@@ -103,13 +108,14 @@ if IS_HEROKU_APP:
         ),
     }
 else:
-
+    # When running locally in development or in CI, a sqlite database file will be used instead
+    # to simplify initial setup. Longer term it's recommended to use Postgres locally too.
     DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
 
 
 # Password validation
